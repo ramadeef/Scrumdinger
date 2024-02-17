8110167aa5d489fd4d6fa9dcf1419615ed4c60cd
Ramesh Adeef <m.a.r.adeef@gmail.com>
2024-02-17 11:29:14 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ramesh Adeef on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
